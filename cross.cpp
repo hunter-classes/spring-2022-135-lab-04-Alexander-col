56ce0dbd768cc898631d916d9a784760743e73f4
Alexander C <83742533+Alexander-col@users.noreply.github.com>
2022-02-25 05:57:08 +0000

    task D working and operating!

--- cross.cpp
+++ cross.cpp
@@ -2,33 +2,6 @@
 
 int main()
 {
-  int height;
-  int hCount = 0;
-  std::cin >> height;
-  int dis = (height - 2);
-  int fordis = 0;
-
-  while (hCount != height)
-  {
-    std::cout << "X";
-    if (dis > 0)
-      {
-        for (int i= 0; i != fordis; i++)
-          {
-            std::cout << " ";
-          }
-        for (int i= 0; i != dis; i++)
-          {
-            std::cout << " ";
-          }
-      }
-      std::cout << "X";
-      std::cout << "\n" << std::endl;
-      dis--;
-      hCount++;
-      fordis++;
-  }
-
-
-  return 0;
+  
 }
+std::string box(int height):
