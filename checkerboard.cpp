81f0727a4e55f1e469c31128c9b7aefc3323199a
Alexander C <83742533+Alexander-col@users.noreply.github.com>
2022-02-25 02:06:09 +0000

    almost done with basic version of task b

--- checkerboard.cpp
+++ checkerboard.cpp
@@ -10,7 +10,7 @@
 
 // Input height: 6
 
- 
+ // Planning to do if divisable by 2 then it will appear a star 
 
 // Shape:
 // * * * * * *
@@ -27,8 +27,56 @@ int main()
     int height; 
     int wCount = 0;
     int hCount = 0;
+    int alt = 0;
+    int count = 0;
+
+    std::cin >> height;
     std::cin >> width;
-    std::cout << (width/2) << "\n";
+
+    while (hCount != height)
+    {
+        if (alt%2 == 0)
+        {
+            while(width != wCount)
+            {
+                if (count%2 == 0)
+                {
+                    std::cout << "*";
+
+                }else
+                {
+                    std::cout << " ";
+                }
+                count++;
+                wCount++;
+            }
+        }else if (alt%2 == 1)
+        {
+            if (alt%2 == 1)
+            {
+                while (width != wCount)
+                {
+                    if (count%2 == 0)
+                    {
+                        std::cout << " ";
+                    }else
+                    {
+                        std::cout << "*";
+                    }
+                    count++;
+                    wCount++;
+                }
+                
+            }
+
+        }
+        count = 0;
+
+        hCount++;
+        alt++;
+
+    }
+
 
     return 0;
 }
